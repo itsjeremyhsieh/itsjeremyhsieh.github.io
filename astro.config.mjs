6b5d8dd51a57cb142306371d9c10eab2afc2debc
itsjeremyhsieh <jeremy.study0107@gmail.com>
2024-09-26 09:23:49 +0800

    edit tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,39 @@
-import sitemap from '@astrojs/sitemap';
-import svelte from "@astrojs/svelte"
-import tailwind from "@astrojs/tailwind"
-import swup from '@swup/astro';
-import Compress from "astro-compress"
-import icon from "astro-icon"
-import { defineConfig } from "astro/config"
-import Color from "colorjs.io"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import swup from "@swup/astro";
+import Compress from "astro-compress";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+import Color from "colorjs.io";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components"; /* Render the custom directive content */
-import rehypeKatex from "rehype-katex"
-import rehypeSlug from "rehype-slug"
-import remarkDirective from "remark-directive" /* Handle directives */
+import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
+import remarkDirective from "remark-directive"; /* Handle directives */
 import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
-import remarkMath from "remark-math"
-import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs"
-import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs"
-import {parseDirectiveNode} from "./src/plugins/remark-directive-rehype.js";
-import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
-import {remarkExcerpt} from "./src/plugins/remark-excerpt.js";
+import remarkMath from "remark-math";
+import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs";
+import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs";
+import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
+
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 const oklchToHex = (str) => {
-  const DEFAULT_HUE = 250
-  const regex = /-?\d+(\.\d+)?/g
-  const matches = str.string.match(regex)
-  const lch = [matches[0], matches[1], DEFAULT_HUE]
+  const DEFAULT_HUE = 250;
+  const regex = /-?\d+(\.\d+)?/g;
+  const matches = str.string.match(regex);
+  const lch = [matches[0], matches[1], DEFAULT_HUE];
   return new Color("oklch", lch).to("srgb").toString({
     format: "hex",
-  })
-}
+  });
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,10 +43,10 @@ export default defineConfig({
     tailwind(),
     swup({
       theme: false,
-      animationClass: 'transition-swup-',   // see https://swup.js.org/options/#animationselector
-                                            // the default value `transition-` cause transition delay
-                                            // when the Tailwind class `transition-all` is used
-      containers: ['main'],
+      animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
+      // the default value `transition-` cause transition delay
+      // when the Tailwind class `transition-all` is used
+      containers: ["main"],
       smoothScrolling: true,
       cache: true,
       preload: true,
@@ -63,25 +69,40 @@ export default defineConfig({
       CSS: false,
       Image: false,
       Action: {
-        Passed: async () => true,   // https://github.com/PlayForm/Compress/issues/376
+        Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
+      },
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
       },
     }),
   ],
   markdown: {
-    remarkPlugins: [remarkMath, remarkReadingTime, remarkExcerpt, remarkGithubAdmonitionsToDirectives, remarkDirective, parseDirectiveNode],
+    remarkPlugins: [
+      remarkMath,
+      remarkReadingTime,
+      remarkExcerpt,
+      remarkGithubAdmonitionsToDirectives,
+      remarkDirective,
+      parseDirectiveNode,
+    ],
     rehypePlugins: [
       rehypeKatex,
       rehypeSlug,
-      [rehypeComponents, {
-        components: {
-          github: GithubCardComponent,
-          note: (x, y) => AdmonitionComponent(x, y, "note"),
-          tip: (x, y) => AdmonitionComponent(x, y, "tip"),
-          important: (x, y) => AdmonitionComponent(x, y, "important"),
-          caution: (x, y) => AdmonitionComponent(x, y, "caution"),
-          warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+      [
+        rehypeComponents,
+        {
+          components: {
+            github: GithubCardComponent,
+            note: (x, y) => AdmonitionComponent(x, y, "note"),
+            tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+            important: (x, y) => AdmonitionComponent(x, y, "important"),
+            caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+            warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+          },
         },
-      }],
+      ],
       [
         rehypeAutolinkHeadings,
         {
@@ -94,7 +115,7 @@ export default defineConfig({
             tagName: "span",
             properties: {
               className: ["anchor-icon"],
-              'data-pagefind-ignore': true,
+              "data-pagefind-ignore": true,
             },
             children: [
               {
@@ -112,12 +133,15 @@ export default defineConfig({
       rollupOptions: {
         onwarn(warning, warn) {
           // temporarily suppress this warning
-          if (warning.message.includes("is dynamically imported by") && warning.message.includes("but also statically imported by")) {
+          if (
+            warning.message.includes("is dynamically imported by") &&
+            warning.message.includes("but also statically imported by")
+          ) {
             return;
           }
           warn(warning);
-        }
-      }
+        },
+      },
     },
     css: {
       preprocessorOptions: {
@@ -129,4 +153,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
